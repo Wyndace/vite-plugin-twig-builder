6533abc083160a1261382fe468d6268a198b111d
Aleksandr Vasilev (wyndace) <wyndace.av@ya.ru>
2025-05-23 21:30:59 +0300

    feat: add all necessary function for generating twig files

---
+++ src/functions/render.ts
@@ -0,0 +1,61 @@
+import twigPkg from 'twig';
+const { renderFile } = twigPkg;
+import fs from 'fs';
+import path from 'path';
+
+function isFileWithSpecificExtensions(filePath: string, extensions: string[]) {
+    return extensions.some((extension) => filePath.endsWith(extension));
+}
+function getFilesWithSpecificExtensionsInDir(dir: string, extensions: string[]): string[] {
+    const files = fs.readdirSync(dir);
+    const matchedFiles = [];
+    for (const file of files) {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            matchedFiles.push(...getFilesWithSpecificExtensionsInDir(filePath, extensions));
+        }
+        else {
+            if(isFileWithSpecificExtensions(filePath, extensions)) {
+                matchedFiles.push(filePath);
+            }
+        }
+    }
+    return matchedFiles;
+}
+
+
+
+function renderTwigFile(filePath: string, base?: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        renderFile(filePath, {
+            settings: {
+                views: base,
+                "twig options": null,
+            }
+        }, (err, html) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(html);
+          }
+        })
+    });
+}
+
+export async function renderAndWriteFilesInDir(dir: string, extensions: string[], outDir: string, base?: string) {
+    const files = getFilesWithSpecificExtensionsInDir(dir, extensions);
+    for (const filePath of files) {
+        console.log(`Rendering ${filePath}...`);
+        const html = await renderTwigFile(filePath, base);
+        const relative = path.relative(dir, filePath);
+        const outPath = path.join(outDir, relative
+            .replace(/^src\/pages\//, '')
+            .replace(/\.twig\.html$/, '.html')
+            .replace(/\.twig$/, '.html'));
+
+        fs.mkdirSync(path.dirname(outPath), { recursive: true });
+        fs.writeFileSync(outPath, html, 'utf8');
+        console.log(`Written: ${outPath}`);
+    }
+}
