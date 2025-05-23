62ca504f54ceeeb01bb95dd5d82aded3f8a7aba9
Aleksandr Vasilev (wyndace) <wyndace.av@ya.ru>
2025-05-23 21:31:27 +0300

    feat: add main vite plugin function

---
+++ src/entry.ts
@@ -0,0 +1,29 @@
+import TwigPagesConfig from "./types/TwigPagesConfig.js";
+import {renderAndWriteFilesInDir} from "./functions/render.js";
+import {Plugin, ResolvedConfig} from 'vite';
+
+const defaultConfig: TwigPagesConfig = {
+    root: './src',
+    dir: './src/pages',
+    outDir: './dist',
+    extensions: ['.twig', '.twig.html'],
+}
+
+export default function twigPages(userConfig: TwigPagesConfig = defaultConfig): Plugin {
+    const config = { ...defaultConfig, ...userConfig };
+    let viteConfig: ResolvedConfig;
+    return {
+        name: 'vite-plugin-twig-pages',
+        configResolved(resolvedConfig) {
+            viteConfig = resolvedConfig;
+        },
+
+        async generateBundle(){
+            const root = config.root || viteConfig.root; // viteConfig.root — абсолютный путь
+            const outDir = config.outDir || viteConfig.build.outDir;
+            const dir = config.dir || root;
+            const extensions = config.extensions;
+            await renderAndWriteFilesInDir(dir, extensions, outDir, root);
+        }
+    }
+}
